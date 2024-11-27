f1417b0e9d065587472edb77fa9b9562a9306f49
Patricio Avila <p.avil;af1998@gmail.com>
2024-11-27 19:02:06 -0300

    setting uo mongoodb to connect it with express

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import connectDB from "./lib/connectDB.js";
+import userRouter from "./routes/user.route.js"
+// import postRouter from "./routes/post.route.js";
+// import commentRouter from "./routes/comment.route.js";
+
+
+const app =  express()
+console.log(process.env.MONGO)
+
+// app.get("/test", (req, res) => {
+//     res.status(200).send("It works")
+// })
+
+app.use("/users", userRouter);
+// app.use("/posts", postRouter);
+// app.use("/comments", commentRouter);
+
+app.listen(3000, () => {
+    connectDB();
+    console.log('Server is Running')
+})
